6024afecf290ef8e7dceedec1b17ac1b19e3f9f3
Nguyen Hong Linh <linh.nh@dgwasia.com>
2024-07-29 11:27:30 +0700

    Developer page and apps page

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/*!
+* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
